a742ede9680018073d020cfc7c1be35b11bf0be0
Luis Cabrera Benito <parzibyte@gmail.com>
2019-10-22 18:00:29 -0500

    Obtener query string con phroute
    También cambiando título

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 require __DIR__ . '/vendor/autoload.php'; #Cargar todas las dependencias
 
 use Parzibyte\Servicios\Comun;
+use Parzibyte\Servicios\Seguridad;
+use Parzibyte\Servicios\SesionService;
 use Parzibyte\Servicios\Twig;
 use Phroute\Phroute\Dispatcher;
 use Phroute\Phroute\Exception\HttpMethodNotAllowedException;
 use Phroute\Phroute\Exception\HttpRouteNotFoundException;
 use Valitron\Validator as V;
+
 V::langDir(__DIR__ . "/vendor/vlucas/valitron/lang");
 V::lang('es');
 define("DIRECTORIO_RAIZ", __DIR__);
@@ -15,7 +18,7 @@ define("RUTA_LOGS", __DIR__ . DIRECTORY_SEPARATOR . "logs");
 define("URL_RAIZ", Comun::env("URL_RAIZ"));
 define("URL_DIRECTORIO_PUBLICO", URL_RAIZ . "/public");
 define("RUTA_API", URL_RAIZ . "/api");
-define("NOMBRE_APLICACION", "App de notas");
+define("NOMBRE_APLICACION", "Plantilla PHP");
 define("AUTOR", "Luis Cabrera Benito a.k.a parzibyte");
 define("WEB_AUTOR", "https://parzibyte.me/blog");
 ini_set('display_errors', 0);
@@ -59,6 +62,12 @@ try {
 }
 function parsearUrl($uri)
 {
+    // Para eliminar la query string pero seguir accediendo a ella a través de $_GET
+    $posicionSignoDeInterrogacion = strpos($uri, "?");
+    if ($posicionSignoDeInterrogacion !== false) {
+        $uri = substr($uri, 0, $posicionSignoDeInterrogacion);
+    }
+
     return implode('/',
         array_slice(
             explode('/', $uri), Comun::env("OFFSET_RUTAS")));
